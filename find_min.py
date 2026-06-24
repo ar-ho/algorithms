99eed4196e43bf72d3ff4993e8db6dee9890755a
ar-ho <horoba@t-online.de>
2026-06-24 11:07:44 +0200

    typos

--- find_min.py
+++ find_min.py
@@ -7,7 +7,6 @@ Implement the "find minimum" algorithm in Python by completing the find_minimum(
     If the list is empty, return None.
     For each number in the list nums, compare it to minimum. If the number is smaller than minimum, set minimum to that number.
     minimum is now set to the smallest number in the list. Return it.
-
 '''
 
 def find_minimum(nums: list[int]) -> float | None:
--- mystery_algo.py
+++ mystery_algo.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
 '''
-
     Start with input variables a and b
     Add a and b using the + operator, and assign the result to a new variable, sum
     Return the sum variable
-
-
 '''
 def summed(nums: list[int]) -> int:
     sum = 0
