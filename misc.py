9eefe60f0a8c130f26228fdb5586c5d5028386c9
ar-ho <horoba@t-online.de>
2026-06-29 17:05:09 +0200

    updated misc.py

--- misc.py
+++ misc.py
@@ -12,3 +12,10 @@ def get_influencer_score(
     num_followers: int, average_engagement_percentage: float
 ) -> float:
     return average_engagement_percentage * math.log(num_followers, 2)
+
+#exponential decay
+def decayed_followers(
+    initial_followers: int, fraction_lost_daily: float, days: int
+) -> float:
+    res = initial_followers * (1 - fraction_lost_daily) ** days
+    return res
